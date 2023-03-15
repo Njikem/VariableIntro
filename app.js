f8f6f19892a120680ab0bd766195823a5afacd15
Njikem <123434988+Njikem@users.noreply.github.com>
2023-03-15 13:14:23 -0600

    variable

---
+++ app.js
@@ -0,0 +1,9 @@
+let price_one = 30;
+let price_two =35;
+let name_one = jonh;
+let name_two = bright;
+
+console.log(price_one);
+console.log(price_two);
+console.log(name_one);
+console.log(name_two);
